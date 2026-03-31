b0012f7c23096622084cd0a4729272b896c86188
claudiadelgado-maths <inmaculadaclaudiadelgado@gmail.com>
2026-03-31 11:54:27 +0200

    update31mrz

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const botonJugar = document.getElementById("play-mathgame");
+    const botonRepositorio = document.getElementById("open-repository");
 
     botonJugar.addEventListener("click", () => {
 
         window.location.href = "games/games.html";
+
+    });
+
+    botonRepositorio.addEventListener("click", () => {
+
+        window.location.href = "repositorio/repositorio.html";
 
     });
 
